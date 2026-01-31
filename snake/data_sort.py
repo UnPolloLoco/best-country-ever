127f767dfed58a9ced853c03ba6ec93e738128b7
Luka <lukayuzvenko@gmail.com>
2026-01-31 09:12:59 -0500

    Cut outdated mapping code

--- snake/data_sort.py
+++ snake/data_sort.py
@@ -2,7 +2,6 @@ from extras import relPath, getJson, getCSV
 
 # Options
 
-show_map = not True
 write_output = True
 
 # Read data
@@ -40,28 +39,14 @@ for n, item in enumerate(sorted_data):
     value = item[-1][:5]
     total += float(value)
 
-    entry_output = ''
-
-    if show_map:
-        if n < 3 or n >= len(sorted_data) - 3 or code == 'USA':
-            if code == 'USA': warn = '>'
-            entry_output = ' '.join((
-                warn, 
-                str(place).ljust(5), 
-                item[1].ljust(45),
-                value,
-                f'{data_percentage}%'.ljust(7),
-            ))
-    
-    else:
-        entry_output = ' '.join((
-            warn, 
-            str(place).ljust(5), 
-            item[1].ljust(45),
-            value,
-            f'{data_percentage}%'.ljust(7),
-            'X' * round(float(value)),
-        ))
+    entry_output = ' '.join((
+        warn, 
+        str(place).ljust(5), 
+        item[1].ljust(45),
+        value,
+        f'{data_percentage}%'.ljust(7),
+        'X' * round(float(value)),
+    ))
 
     full_output += entry_output + '\n'
     
@@ -81,76 +66,8 @@ print()
 average = total / country_count
 median = sorted_data[round(country_count / 2)][-1]
 
-if not show_map:
-    print('  Countries:', country_count)
-    print()
+print('  Countries:', country_count)
+print()
 
-    print('  Average:  ', round(average))
-    print()
-
-# Mapping
-
-if show_map:
-    # Get/sort mapping data
-
-    coords = getCSV('../otherData/coords.csv')[1:]
-
-    sorted_coords = sorted(coords, key=(lambda x: float(x[-2])))
-    sorted_coords.reverse()
-
-    # Make lat dict
-
-    latitude_dict = {i: [] for i in range(-90, 90)}
-    for i in sorted_coords:
-        lat = round(float(i[-2]))
-        latitude_dict[lat].append(i)
-
-    # Sort lat dict by lon
-
-    for i in latitude_dict:
-        sorted_lon = sorted(latitude_dict[i], key=(lambda x: float(x[-1])))
-        latitude_dict[i] = sorted_lon
-
-    # Map generator
-
-    full_map = {}
-
-    for lat in range(-90, 90):
-        full_map[lat] = {}
-        lat_data = latitude_dict[lat]
-
-        item_on_lat = 0
-        
-        for lon in range(-180, 180):
-            full_map[lat][lon] = [-1, None]
-
-            if item_on_lat + 1 <= len(lat_data):
-                # If there exists any remaining countries on this latitude...
-
-                item = latitude_dict[lat][item_on_lat]
-
-                if round(float(item[-1])) == lon:
-                    item_on_lat += 1
-                    full_map[lat][lon] = [0, item[1]]
-
-    # Map drawer
-
-    for lat in range(89, -91, -1):
-        line = ''
-        for lon in range(-180, 180):
-            if full_map[lat][lon][0] == -1:
-                line += '  '
-            else:
-                code = full_map[lat][lon][1]
-
-                if code in data_dict:
-                    value = float(data_dict[code][-1])
-                    if value > 60:
-                        line += 'XX'
-                    else:
-                        line += '..'
-                else:
-                    line += '. '
-        
-        print(line)
-    
+print('  Average:  ', round(average))
+print()
